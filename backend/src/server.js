d67a0c2ddb216158eadfea53b0d673bf5253dfac
thienphuprogrammer <thienphuprogrammer@gmail.com>
2023-11-14 15:09:25 +0700

    -1

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,8 +35,7 @@ app.get('*', (req, res) => {
     res.sendFile(indexFilePath);
 });
 
-const PORT = 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
-    console.log(`http://localhost:${PORT}/api/foods`);
 });
